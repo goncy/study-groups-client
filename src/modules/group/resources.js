bb88b45d51d687ed0677219dce27f5ad98ea7aaf
goncy <gonzalo.pozzo4@gmail.com>
2018-10-27 15:42:52 -0300

    Change class for assignment

--- src/modules/group/resources.js
+++ src/modules/group/resources.js
@@ -4,9 +4,9 @@ const URL = "http://localhost:8888/groups";
 
 export default {
   search: (university, assignment) =>
-    axios(`${URL}?university_like=${university}&class_like=${assignment}`).then(
-      resp => resp.data
-    ),
+    axios(
+      `${URL}?university_like=${university}&assignment_like=${assignment}`
+    ).then(resp => resp.data),
   fetch: id => axios(`${URL}/${id}`),
   create: data =>
     axios({
