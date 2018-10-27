f3b8c387e72e473250797ed24ce72d4e0a233870
Guadalupe Jazmin Lazzo <guadalupe.lazzo@mercadolibre.com>
2018-10-27 11:59:13 -0300

    login with styles

--- src/modules/user/screens/Login.js
+++ src/modules/user/screens/Login.js
@@ -1,15 +1,64 @@
 // @flow
-import type {Container} from "unstated";
-
+import type {ContainerType} from "unstated";
+import styled from "styled-components";
 import React, {Component} from "react";
 import connect from "unstated-connect";
-
+import {TextInput, Button} from "evergreen-ui";
 import UserContainer from "../container";
 
 type Props = {
-  containers: Array<Container>,
+  containers: Array<ContainerType>,
 };
-
+const Container = styled.div`
+  font-family: "Open Sans", sans-serif;
+  background: #d1196e;
+  width: 100%;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Section = styled.div`
+  background: #192a3a;
+  width: 70%;
+  height: 50%;
+  display: flex;
+  -webkit-box-shadow: 0px -1px 44px -2px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 0px -1px 44px -2px rgba(0, 0, 0, 0.75);
+  box-shadow: 0px -1px 44px -2px rgba(0, 0, 0, 0.75);
+`;
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 50%;
+  height: 91%;
+  margin: 2em;
+`;
+const Presentation = styled.div`
+  font-family: "Open Sans", sans-serif;
+  background: #34cea7;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+  height: 100%;
+  h4 {
+    margin: 0;
+    font-size: 1.6em;
+    color: #192a3a;
+  }
+  p {
+    text-align: center;
+    color: #192a3a;
+    max-width: 80%;
+    font-size: 20px;
+  }
+`;
 class LoginScreen extends Component {
   props: Props;
 
@@ -36,22 +85,40 @@ class LoginScreen extends Component {
     } = this.props;
 
     return (
-      <div>
-        <form onSubmit={this.login}>
-          <input
-            type="email"
-            value={email}
-            onChange={e => this.setState({email: e.target.value})}
-          />
-          <input
-            type="password"
-            value={password}
-            onChange={e => this.setState({password: e.target.value})}
-          />
-          <button type="submit">Login</button>
-        </form>
-        {JSON.stringify(user.state)}
-      </div>
+      <Container>
+        <Section>
+          <Presentation>
+            <h4>Study Meeting</h4>
+            <p>Buscá grupos de estudio que estén estudiando lo mismo que vos</p>
+          </Presentation>
+          <Form onSubmit={this.login}>
+            <TextInput
+              margin="1em"
+              placeholder="Email"
+              type="email"
+              value={email}
+              onChange={e => this.setState({email: e.target.value})}
+            />
+            <TextInput
+              margin="1em"
+              placeholder="Password"
+              type="password"
+              value={password}
+              onChange={e => this.setState({password: e.target.value})}
+            />
+            <Button
+              appearance="primary"
+              intent="success"
+              margin="1em"
+              type="submit"
+            >
+              Logueate
+            </Button>
+          </Form>
+        </Section>
+
+        {/* JSON.stringify(user.state) */}
+      </Container>
     );
   }
 }
