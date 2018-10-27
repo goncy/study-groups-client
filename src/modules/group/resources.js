6e0bda9f0a7fad468bd42872c3cb4107f6200c7c
Micael Robles <mrobles@litebox.ai>
2018-10-27 11:49:20 -0300

    progreso detail

--- src/modules/group/resources.js
+++ src/modules/group/resources.js
@@ -1,21 +1,20 @@
-import axios from "axios";
+import axios from 'axios'
 
-const URL = "http://localhost:8888/groups";
+const URL = 'http://localhost:8888/groups'
 
 export default {
-  dogs: {
-    search: criteria => axios(`${URL}?name_like=${criteria}`),
-    create: data =>
-      axios({
-        data,
-        url: URL,
-        method: "POST",
-      }),
-    update: data =>
-      axios({
-        data,
-        url: URL,
-        method: "PATCH",
-      }),
-  },
-};
+  search: criteria => axios(`${URL}?name_like=${criteria}`),
+  fetch: id => axios(`${URL}/${id}`),
+  create: data =>
+    axios({
+      data,
+      url: URL,
+      method: 'POST',
+    }),
+  update: data =>
+    axios({
+      data,
+      url: URL,
+      method: 'PATCH',
+    }),
+}
